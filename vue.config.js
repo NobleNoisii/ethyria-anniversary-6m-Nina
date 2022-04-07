7398c2ce9795b8aa73cc9757d75f1d31eccbff49
dragonjet <jetriconew@gmail.com>
2022-04-07 02:55:01 -0700

    [build]

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = defineConfig({
         /(.*).js.map/g,
       ],
     },
-    assetsVersion: '2022_04_07_00_00',
+    assetsVersion: '2022_04_07_02_00',
     manifestOptions: {
       name: 'Nina 6 Months and 300K',
       short_name: 'Nina 6m and 300K',
