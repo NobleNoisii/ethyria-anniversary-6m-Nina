294a80caa57e15a1591c7cdd2f5de816fd9b0361
dragonjet <jetriconew@gmail.com>
2022-04-07 23:40:13 -0700

    Updated credits

--- src/components/SectionCredits.vue
+++ src/components/SectionCredits.vue
@@ -2,7 +2,19 @@
   <v-container>
     <v-row no-gutters class="mb-4">
       <v-col cols="12" class="text-center white--text">
-        For Nina Kosaka. From your honeys.
+        <p>For Nina Kosaka. From your honeys.</p>
+        <p>
+          Management: Ninacord Staff<br />
+          Programming: jetrico<br />
+          Header, Background, Card Design: Professor Nobu<br />
+          God Sees All (kazoo cover): Altair<br />
+          Drawing Board: jetrico, Professor Nobu, NobleNoisii, KAO,
+            AngelCokeLife, Altair, Clover, Komet keiko, Voldox,
+            Trey, Tabitha, Premierner, Cerise, TaCrimson
+        </p>
+        <p>
+          and all honeys who celebrate these milestones with Nina and Ethyria!
+        </p>
       </v-col>
     </v-row>
   </v-container>
